bed504e5170c2b5631542875d9a6f09dff56cc6f
Pascal Weber <pascal.weber@prodyna.com>
2019-11-03 14:52:02 +0100

    define font for info i

--- src/pages/AngeboteDienstleistungen/TabPanelAngebote.js
+++ src/pages/AngeboteDienstleistungen/TabPanelAngebote.js
@@ -36,7 +36,7 @@ const StyledAngebote = styled.div`
       left: 0;
       content: "i";
       font-size: 98px;
-      font-family: serif;
+      font-family: Times New Roman;
       color: #8abbe2;
       font-style: italic;
       transition: all 0.6s ease;
